b93fbcfccc5d1d98c510f068f9cb7d770efd1915
jauharnotes <jauharnotes@gmail.com>
2022-02-21 21:17:40 +0700

    materi useRef hook

---
+++ hook/useRef.js
@@ -0,0 +1,30 @@
+const MyApp = () => {
+
+  const [judul, setJudul] = React.useState("Belajar Ract");
+  let foo = React.useRef("Hei")
+  console.log("Rerender");
+
+  const handleButtonClick = () => {
+      foo.current = "Hello";
+      if (judul === "Belajar React") {
+          setJudul("Belajar Vue");
+    } else {
+      setJudul("Belajar React");
+    }
+  };
+
+  const handleFooClick = () => {
+      console.log(foo);
+      console.log(foo.current);
+  }
+
+  return (
+    <>
+      <h1>{judul}</h1>
+      <button onClick={handleButtonClick}>Click Me!</button>
+      <button onClick={handleFooClick}>Get Foo</button>
+    </>
+  );
+};
+
+ReactDOM.render(<MyApp />, document.getElementById("root"));
