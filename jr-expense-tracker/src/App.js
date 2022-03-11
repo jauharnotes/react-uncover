8de932421c457a24649b4b24f690fd0c148fb4ad
jauharnotes <jauharnotes@gmail.com>
2022-03-11 18:11:53 +0700

    project deploy

--- jr-expense-tracker/src/App.js
+++ jr-expense-tracker/src/App.js
@@ -1,57 +1,81 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Transaction from "./components/Transaction";
 import SaldoBox from "./components/SaldoBox";
 import AddTransaction from "./components/AddTransaction";
 
-const initTransactions = [
-    {
-        "id": "619941539079",
-        "tanggal": new Date("01 Nov 2022 9:30").getTime(),
-        "keterangan": "Gaji bulanan",
-        "nominal": 2500000,
-    },
-    {
-        "id": "749179155708",
-        "tanggal": new Date("01 Nov 2022 10:00").getTime(),
-        "keterangan": "Uang Lembur",
-        "nominal": 750000,
-    },
-    {
-        "id": "568004092688",
-        "tanggal": new Date("01 Nov 2022 10:30").getTime(),
-        "keterangan": "Beli sepatu",
-        "nominal": -150000,
-    },
-];
+let url = "https://jr-expense-trackers-default-rtdb.asia-southeast1.firebasedatabase.app/transaction.json";
 
 const App = () => {
-    const [transactions, setTransaction] = useState(initTransactions);
+    const [transactions, setTransaction] = useState([]);
+    const [submitCount, setSubmiteCount] = useState(0);
+
+    // Use effect untuk mengakses data dari API firebase
+    useEffect(() => {
+      const myFetch = async () => {
+        try {
+          let response = await fetch(url);
+          if (!response.ok) {
+            throw new Error(response.status);
+          }
+          let responseData = await response.json();
+
+          let initTransactions = [];
+          for (const key in responseData) {
+            initTransactions.push({
+              id: key,
+              tanggal: responseData[key].tanggal,
+              keterangan: responseData[key].keterangan,
+              nominal: responseData[key].nominal,
+            })
+          }
+          // atur ulang transaction agar tanggal terkecil di bagian atas
+          initTransactions.sort((a, b) => a.tanggal - b.tanggal);
+
+          setTransaction(initTransactions)
+        } catch (error) {
+          console.log(`Terjadi gangguan dengan pesan : "${error}"`);
+        }
+      }
+      myFetch();
+    }, [submitCount]);
     
     // handler untuk menambah transaction,
     // akan di-triger dari komponen AddTransaction
-    const hanleTambahTransaction = (data) => {
-      let newTransactions = [
-        ...transactions, data
-      ];
-
-      // atur ulang urutan transaction agar tanggal terkecil di bagi atas
-      newTransactions.sort((a, b) => a.tanggal - b.tanggal);
-
-      setTransaction(newTransactions);
+    const hanleTambahTransaction = async (data) => {
+      // kirim data ke server (firebase)
+      try {
+        let response = await fetch(url, {
+          method: "POST",
+          body: JSON.stringify(data)
+        })
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        setSubmiteCount(submitCount + 1);
+      } catch (error) {
+        console.log(`Terjadi gangguan dengan pesan : "${error}"`);
     }
+  }
 
     // handler untuk menghapus data transaction di komponen AddTransaction
-    const handleHapusTransaction = (e) => {
-      // cari index transaction yang akan dihapus berdasarkan id
-      const result = transactions.findIndex(
-        transaction => (transaction.id === e.target.id)
-      );
-      // copy transaction karena fungsi splice akan mengubah array asal (mutate)
-      const newTransactions = transactions;
-      newTransactions.splice(result, 1);
-      setTransaction([...newTransactions]);
+    const handleHapusTransaction = async (e) => {
+      // Rangkai alamat URL agar berisi id data yang dihapus
+      let url = `https://jr-expense-trackers-default-rtdb.asia-southeast1.firebasedatabase.app/transaction/${e.target.id}.json`;
+
+      // Kirim delete request ke server (firebase)
+      try {
+        let response = await fetch(url, {
+          method: "DELETE",
+        })
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        setSubmiteCount(submitCount + 1);
+      } catch (error) {
+        console.log(`Terjadi gangguan dengan pesan : "${error}"`);
+      }
     }
     
   return (
